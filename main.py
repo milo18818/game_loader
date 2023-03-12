191f21321ba1a8df195fb06fb5f15d38e322472a
milo18818 <vippugster@gmail.com>
2023-03-12 12:12:41 +0000

    added number game

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import sys
 sys.path.insert(1,"turtle_racing")
 
+sys.path.insert(2,"number_game")
+
+from number_game import number_game
+
 from turtle_race import turtle_race
 
-turtle_race()
+
+
+number_game()
